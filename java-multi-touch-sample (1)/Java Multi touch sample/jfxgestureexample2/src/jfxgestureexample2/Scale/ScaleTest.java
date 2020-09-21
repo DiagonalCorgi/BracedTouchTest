16f5a9c4229a3bb9f22e16057ee1557bd2fba61c
Dylan Carlyle <dca87@uclive.ac.nz>
2020-09-22 11:59:43 +1200

    added the scale test

---
+++ java-multi-touch-sample (1)/Java Multi touch sample/jfxgestureexample2/src/jfxgestureexample2/Scale/ScaleTest.java
@@ -0,0 +1,34 @@
+package jfxgestureexample2.Scale;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ScaleTest extends Application{
+
+
+
+
+    public static void main(String[] args){
+       launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("ScaleTest.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Scale Example");
+        primaryStage.show();
+
+
+
+
+    }
+
+
+
+}
